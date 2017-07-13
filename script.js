76b4ffd5b0f80b67df6aeeab1262b40c8a7ea80e
Denis4911 <deniskamotskiy@ro.ru>
2017-07-14 02:52:51 +0500

    oleg2

---
+++ script.js
@@ -0,0 +1,12 @@
+//Плавный скролл для всех ссылок А
+
+// $(document).ready(function() {
+// $('a[href^="#"]').click(function(){
+// var el = $(this).attr('href');
+// $('body').animate({
+// scrollTop: $(el).offset().top}, 1000);
+// return false;
+// });
+// });
+
+
