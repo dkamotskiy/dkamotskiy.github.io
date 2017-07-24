cb360aa5505067a1de15f3a27906a01a20639bc0
Denis4911 <deniskamotskiy@ro.ru>
2017-07-25 02:25:57 +0500

    1

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+
+$("#contactsbutton").mouseenter(function() {
+	$("#contacti").css({'display' : 'block'});
+});
+
+$("#contactsbutton").mouseleave(function() {
+	$("#contacti").css({'display' : 'none'});
+});
+
+$("#contacti").mouseenter(function() {
+	$(this).css({'display' : 'block'});
+});
+
+$("#contacti").mouseleave(function() {
+	$(this).css({'display' : 'none'});
+});
+
+
+
+
