32becb7c4478143bb5f51e6200a59000a248db57
Denis4911 <deniskamotskiy@ro.ru>
2017-08-08 13:53:48 +0500

    11

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-  $('#try').each(function(){
-	var height1 = $(this).height();
-	$(this).find('.col-md-1').css('height', height1);
-});
-$(window).resize(function() {
-	$('#try').each(function(){
-	var height2 = $(this).height();
-	$(this).find('.col-md-1').css('height', height2);
-	var height3 = $('#colonka').height();
-	$(this).find('.col-md-1').css('height', height3);
-});
+//   $('#try').each(function(){
+// 	var height1 = $(this).height();
+// 	$(this).find('.col-md-1').css('height', height1);
+// });
+// $(window).resize(function() {
+// 	$('#try').each(function(){
+// 	var height2 = $(this).height();
+// 	$(this).find('.col-md-1').css('height', height2);
+// 	var height3 = $('#colonka').height();
+// 	$(this).find('.col-md-1').css('height', height3);
+// });
 
-});
+// });
 $(document).ready(function() {
 	$(".phone-number").mask("8(999) 999-9999");
 });
