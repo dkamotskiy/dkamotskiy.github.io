aead0be11b74c3efdc3d8a91071f602688124107
Denis4911 <deniskamotskiy@ro.ru>
2017-08-16 23:05:40 +0500

    врем

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$('select').each(function(){
+	$(this).siblings('p').text( $(this).children('option:selected').text() );
+});
+$('select').change(function(){
+	$(this).siblings('p').text( $(this).children('option:selected').text() );
+});
+	$(document).ready(function() {
+    $("a[rel=example_group]").fancybox({
+        'transitionIn' : 'none',
+        'transitionOut' : 'none',
+        'titlePosition' : 'over',
+        'titleFormat' : function(title, currentArray, currentIndex, currentOpts) {
+            return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+        }
+    });
+	});
