77f0d4095b4e389cb66384f27b33dfe621e6a60a
Denis4911 <deniskamotskiy@ro.ru>
2017-08-18 13:05:10 +0500

    11

--- js/swipe.js
+++ js/swipe.js
@@ -1,15 +1,15 @@
-$(".carousel").on("touchstart", function(event){
+$("#carousel-example-generic").on("touchstart", function(event){
         var xClick = event.originalEvent.touches[0].pageX;
     $(this).one("touchmove", function(event){
         var xMove = event.originalEvent.touches[0].pageX;
         if( Math.floor(xClick - xMove) > 5 ){
-            $(".carousel").carousel('next');
+            $("#carousel-example-generic").carousel('next');
         }
         else if( Math.floor(xClick - xMove) < -5 ){
-            $(".carousel").carousel('prev');
+            $("#carousel-example-generic").carousel('prev');
         }
     });
-    $(".carousel").on("touchend", function(){
+    $("#carousel-example-generic").on("touchend", function(){
             $(this).off("touchmove");
     });
 });
