062556599cf12cccaa278c9d2bf1bdafe6bd65dc
Denis4911 <deniskamotskiy@ro.ru>
2017-08-19 15:44:30 +0500

    111

--- js/script.js
+++ js/script.js
@@ -4,16 +4,16 @@ $('select').each(function(){
 $('select').change(function(){
 	$(this).siblings('p').text( $(this).children('option:selected').text() );
 });
-	// $(document).ready(function() {
- //    $("a[rel=example_group]").fancybox({
- //        'transitionIn' : 'none',
- //        'transitionOut' : 'none',
- //        'titlePosition' : 'over',
- //        'titleFormat' : function(title, currentArray, currentIndex, currentOpts) {
- //            return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
- //        }
- //    });
-	// });
+	$(document).ready(function() {
+    $("a[rel=example_group]").fancybox({
+        'transitionIn' : 'none',
+        'transitionOut' : 'none',
+        'titlePosition' : 'over',
+        'titleFormat' : function(title, currentArray, currentIndex, currentOpts) {
+            return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+        }
+    });
+	});
 $(document).ready(function(){
     $( "#button-novostroyka" ).click(function() {
         $("#button-kapital").attr("style","border-bottom: 3px solid transparent;");
