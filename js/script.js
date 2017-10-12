d9afef5a6b1fdbc976f970a7b77970a5c69fef29
Denis4911 <deniskamotskiy@ro.ru>
2017-10-12 16:00:29 +0500

    ll

--- js/script.js
+++ js/script.js
@@ -1,98 +1,316 @@
-// прилипание меню
-$(function(){
- $(window).scroll(function() {
- if($(this).scrollTop() >= 760) {
- $('#menu').addClass('stickytop');
- }
- else{
- $('#menu').removeClass('stickytop');
- }
- });
-});
-$(function(){
- $(window).scroll(function() {
- if($(this).scrollTop() >= 560) {
- $('#menu-adaptive').addClass('stickytop');
- }
- else{
- $('#menu-adaptive').removeClass('stickytop');
- }
- });
-});
-// якори
+//Логотип
 $(document).ready(function(){
-	$("#menu").on("click","a", function (event) {
-		//отменяем стандартную обработку нажатия по ссылке
-		event.preventDefault();
-
-		//забираем идентификатор бока с атрибута href
-		var id  = $(this).attr('href'),
-
-		//узнаем высоту от начала страницы до блока на который ссылается якорь
-			top = $(id).offset().top-120;
-		
-		//анимируем переход на расстояние - top за 1500 мс
-		$('body,html').animate({scrollTop: top}, 1500);
-	});
+$(".logo-ekran1").animate({bottom: "50%"}, 1000).delay(1000);
+});
+// Инициализация скроллинга
+$('#fullpage').fullpage({
+    anchors:['firstSlide','secondSlide','3rdSlide','4thSlide','5thSlide','6thSlide','7thSlide','8thSlide','9thSlide'],
+    sectionSelector: '.vertical-scrolling',
+    slideSelector: '.horizontal-scrolling',
+    controlArrows: false,
+    navigation:true,
+    navigationPosition:'left',
+    overflowScroll:true,
+    afterLoad:function(link,index) {
+// первый слайд
+        if(index == 1) {
+          $(".menu-button").css({'color':'#fff'});
+          $(".bar").css({'background-color':'#fff'});
+          $("#fp-nav ul li a span").css({'background-color':'#fff'});
+          $("#fp-nav ul li a span").css({'border':'1px solid #fff'});
+          $("#fp-nav ul li:hover a span").css({'border':'1px solid #fff'});
+          $("#fp-nav ul li a.active span").css({'border':'1px solid #fff'});
+        }
+        if(link == 'firstSlide') {
+         $(".menu-button").css({'color':'#fff'});
+         $(".bar").css({'background-color':'#fff'});
+         $("#fp-nav ul li a span").css({'background-color':'#fff'});
+         $("#fp-nav ul li a span").css({'border':'1px solid #fff'});
+         $("#fp-nav ul li:hover a span").css({'border':'1px solid #fff'});
+         $("#fp-nav ul li a.active span").css({'border':'1px solid #fff'});
+        }
+// второй слайд
+        if(index == 2) {
+          $(".menu-button").css({'color':'black'});
+          $(".bar").css({'background-color':'black'});
+          $("#fp-nav ul li a span").css({'background-color':'#95989A'});
+          $("#fp-nav ul li a span").css({'border':'1px solid #95989A'});
+          $("#fp-nav ul li:hover a span").css({'border':'1px solid #95989A'});
+          $("#fp-nav ul li a.active span").css({'border':'1px solid #95989A'});
+        }
+        if(link == 'secondSlide') {
+         $(".menu-button").css({'color':'black'});
+         $(".bar").css({'background-color':'black'});
+         $("#fp-nav ul li a span").css({'background-color':'#95989A'});
+         $("#fp-nav ul li a span").css({'border':'1px solid #95989A'});
+         $("#fp-nav ul li:hover a span").css({'border':'1px solid #95989A'});
+         $("#fp-nav ul li a.active span").css({'border':'1px solid #95989A'});
+        }
+// третий слайд
+        if(index == 3) {
+          $(".menu-button").css({'color':'#fff'});
+          $(".bar").css({'background-color':'#fff'});
+          $("#fp-nav ul li a span").css({'background-color':'#fff'});
+          $("#fp-nav ul li a span").css({'border':'1px solid #fff'});
+          $("#fp-nav ul li:hover a span").css({'border':'1px solid #fff'});
+          $("#fp-nav ul li a.active span").css({'border':'1px solid #fff'});
+        }
+        if(link == '3rdSlide') {
+         $(".menu-button").css({'color':'#fff'});
+         $(".bar").css({'background-color':'#fff'});
+         $("#fp-nav ul li a span").css({'background-color':'#fff'});
+         $("#fp-nav ul li a span").css({'border':'1px solid #fff'});
+          $("#fp-nav ul li:hover a span").css({'border':'1px solid #fff'});
+          $("#fp-nav ul li a.active span").css({'border':'1px solid #fff'});
+        }  
+// 4 слайд
+        if(index == 4) {
+          $(".menu-button").css({'color':'#000'});
+          $(".bar").css({'background-color':'#000'});
+          $("#fp-nav ul li a span").css({'background-color':'#95989A'});
+          $("#fp-nav ul li a span").css({'border':'1px solid #95989A'});
+          $("#fp-nav ul li:hover a span").css({'border':'1px solid #95989A'});
+          $("#fp-nav ul li a.active span").css({'border':'1px solid #95989A'});
+        }
+        if(link == '4thSlide') {
+         $(".menu-button").css({'color':'#000'});
+         $(".bar").css({'background-color':'#000'});
+         $("#fp-nav ul li a span").css({'background-color':'#95989A'});
+         $("#fp-nav ul li a span").css({'border':'1px solid #95989A'});
+          $("#fp-nav ul li:hover a span").css({'border':'1px solid #95989A'});
+          $("#fp-nav ul li a.active span").css({'border':'1px solid #95989A'});
+        }  
+// 5 слайд
+        if(index == 5) {
+          $(".menu-button").css({'color':'#fff'});
+          $(".bar").css({'background-color':'#fff'});
+          $("#fp-nav ul li a span").css({'background-color':'#fff'});
+          $("#fp-nav ul li a span").css({'border':'1px solid #fff'});
+          $("#fp-nav ul li:hover a span").css({'border':'1px solid #fff'});
+          $("#fp-nav ul li a.active span").css({'border':'1px solid #fff'});
+        }
+        if(link == '5thSlide') {
+         $(".menu-button").css({'color':'#fff'});
+         $(".bar").css({'background-color':'#fff'});
+         $("#fp-nav ul li a span").css({'background-color':'#fff'});
+         $("#fp-nav ul li a span").css({'border':'1px solid #fff'});
+          $("#fp-nav ul li:hover a span").css({'border':'1px solid #fff'});
+          $("#fp-nav ul li a.active span").css({'border':'1px solid #fff'});
+        } 
+// 6 слайд
+        if(index == 6) {
+          $(".menu-button").css({'color':'#000'});
+          $(".bar").css({'background-color':'#000'});
+          $("#fp-nav ul li a span").css({'background-color':'#95989A'});
+          $("#fp-nav ul li a span").css({'border':'1px solid #95989A'});
+          $("#fp-nav ul li:hover a span").css({'border':'1px solid #95989A'});
+          $("#fp-nav ul li a.active span").css({'border':'1px solid #95989A'});
+        }
+        if(link == '6thSlide') {
+         $(".menu-button").css({'color':'#000'});
+         $(".bar").css({'background-color':'#000'});
+         $("#fp-nav ul li a span").css({'background-color':'#95989A'});
+          $("#fp-nav ul li:hover a span").css({'border':'1px solid #95989A'});
+          $("#fp-nav ul li a.active span").css({'border':'1px solid #95989A'});
+        } 
+// 7 слайд
+        if(index == 7) {
+          $(".menu-button").css({'color':'#fff'});
+          $(".bar").css({'background-color':'#fff'});
+          $("#fp-nav ul li a span").css({'background-color':'#fff'});
+          $("#fp-nav ul li a span").css({'border':'1px solid #fff'});
+          $("#fp-nav ul li:hover a span").css({'border':'1px solid #fff'});
+          $("#fp-nav ul li a.active span").css({'border':'1px solid #fff'});
+        }
+        if(link == '7thSlide') {
+         $(".menu-button").css({'color':'#fff'});
+         $(".bar").css({'background-color':'#fff'});
+         $("#fp-nav ul li a span").css({'background-color':'#fff'});
+         $("#fp-nav ul li a span").css({'border':'1px solid #fff'});
+          $("#fp-nav ul li:hover a span").css({'border':'1px solid #fff'});
+          $("#fp-nav ul li a.active span").css({'border':'1px solid #fff'});
+        }       
+      }
+    // другие опции
+});
+// Появление левой кнопки
+$(document).ready(function(){
+$( "#ekran4 .jcarousel-control-next" ).click(function() {
+    $("#ekran4 .jcarousel-control-prev").attr("style","display:block");
+});  
 });
 $(document).ready(function(){
-	$("#menu-adaptive").on("click","a", function (event) {
-		//отменяем стандартную обработку нажатия по ссылке
-		event.preventDefault();
-
-		//забираем идентификатор бока с атрибута href
-		var id  = $(this).attr('href'),
-
-		//узнаем высоту от начала страницы до блока на который ссылается якорь
-			top = $(id).offset().top-130;
-		
-		//анимируем переход на расстояние - top за 1500 мс
-		$('body,html').animate({scrollTop: top}, 1500);
-	});
-});
-// подсветка якорей
-// Cache selectors
-var lastId,
-    topMenu = $("#menu .container .row .col-md-6 div"),
-    topMenuHeight = topMenu.outerHeight()+15,
-    // All list items
-    menuItems = topMenu.find("a"),
-    // Anchors corresponding to menu items
-    scrollItems = menuItems.map(function(){
-      var item = $($(this).attr("href"));
-      if (item.length) { return item; }
-    });
-
-// Bind click handler to menu items
-// so we can get a fancy scroll animation
-menuItems.click(function(e){
-  var href = $(this).attr("href"),
-      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
-  $('html, body').stop().animate({ 
-      scrollTop: offsetTop
-  }, 300);
-  e.preventDefault();
+$( "#ekran6 .jcarousel-control-next" ).click(function() {
+    $("#ekran6 .jcarousel-control-prev").attr("style","display:block");
+});  
 });
 
-// Bind to scroll
+
+
+
+
+
+
+
+
+
+
+//скролл
 $(window).scroll(function(){
-   // Get container scroll position
-   var fromTop = $(this).scrollTop()+topMenuHeight;
-   
-   // Get id of current scroll item
-   var cur = scrollItems.map(function(){
-     if ($(this).offset().top < fromTop)
-       return this;
-   });
-   // Get the id of the current element
-   cur = cur[cur.length-1];
-   var id = cur && cur.length ? cur[0].id : "";
-   
-   if (lastId !== id) {
-       lastId = id;
-       // Set/remove active class
-       menuItems
-         .parent().removeClass("active")
-         .end().filter("[href='#"+id+"']").parent().addClass("active");
-   }                   
+  var offset = $('.white').offset(),
+      offsetwb = $('.white + .black').offset();
+  if ($(this).scrollTop() > offset.top && $(this).scrollTop() < offsetwb.top) {
+    $('.menu-button').css('background-color','black !important');
+  }
+  else {
+    $('.menu-button').css('background-color','white !important');
+  }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Маска
+jQuery(function($){
+   $("#phone-form").mask("+7 (999) 999-9999");
+   $("#phone-forma2").mask("+7 (999) 999-9999");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//GoogleMap
+                  function initMap() {
+                    var uluru = {lat: 54.736908, lng: 55.987691};
+                    var map = new google.maps.Map(document.getElementById('map'), {
+                      zoom: 15,
+                      center: uluru,
+                      scrollwheel: false,
+                      minZoom: 10,
+                      styles: [
+            {elementType: 'geometry', stylers: [{color: '#0A3848'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#0A3848'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ],
+            disableDefaultUI: true,
+                    });
+                    var marker = new google.maps.Marker({
+                      position: uluru,
+                      map: map
+                    });
+                  }
